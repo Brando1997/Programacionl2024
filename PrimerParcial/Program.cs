09cafbd5e30a729f3fca44c3d1a8916c35b9b3a0
Brando Alvarez <105041267+Brando1997@users.noreply.github.com>
2024-03-02 10:25:35 -0600

    Add files via upload

---
+++ PrimerParcial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Desafío_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Desafio1();
+        }
+        public static void Desafio1()
+        {
+            Console.WriteLine("Ingrese Numero Positivo");
+            int numerosABucar = int.Parse(Console.ReadLine());
+            while (numerosABucar !=1)
+            {
+                for(int i = 1; i <(numerosABucar); i++)
+                {
+                    if(i % 2 == 0)
+                    {
+                        Console.WriteLine("Los numeros Pares son {0}", i);
+                    }
+                    if(i % 2 >= 1)
+                    {
+                        continue;
+                    }
+                }
+
+                Console.WriteLine("Ingrese Numero Positivo");
+                numerosABucar = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
